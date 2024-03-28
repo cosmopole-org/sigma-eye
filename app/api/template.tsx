579c737bc7983be885db67e5a56c4ea786db5ba2
Keyhan Mohammadi <hawkcosmopolitan@gmail.com>
2024-03-28 16:54:26 +0330

    .

--- app/api/template.tsx
+++ app/api/template.tsx
@@ -8,8 +8,8 @@ export default function Template({ children }: { children: ReactNode }) {
     const path = usePathname();
     return (
         <motion.div
-            initial={{ x: path.startsWith('/api/home/') ? -50 : 50, opacity: 0 }}
-            exit={{ x: path.startsWith('/api/home/') ? -50 : 50, opacity: 0 }}
+            initial={{ x: path.startsWith('/api/home/') ? 0 : 50, opacity: 0 }}
+            exit={{ x: path.startsWith('/api/home/') ? 0 : 50, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             transition={{ ease: 'easeInOut', duration: 0.25}}
         >
