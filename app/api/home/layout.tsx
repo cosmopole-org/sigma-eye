09e8b7cedcef64e3d78e323d4af804c96c473473
Keyhan Mohammadi <hawkcosmopolitan@gmail.com>
2024-03-28 18:19:44 +0330

    fixed bugs.

--- app/api/home/layout.tsx
+++ app/api/home/layout.tsx
@@ -47,7 +47,7 @@ export default function HomeLayout({
 				{children}
 				{
 					showLoadingState.get({ noproxy: true }) ? (
-						<Card shadow="none" radius="none" className="w-full h-full fixed left-0 top-0" style={{ zIndex: 99 }}>
+						<Card shadow="none" radius="none" className="w-full h-full fixed left-0 top-0" style={{ zIndex: 50 }}>
 							<Card className="w-24 h-24 fixed left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 items-center justify-center">
 								<CircularProgress />
 							</Card>
--- components/home/home-bottomnav.tsx
+++ components/home/home-bottomnav.tsx
@@ -20,7 +20,7 @@ export default function HomeBottomNav() {
         router.push('/api/home/' + key);
     }
     return (
-        <Card className="grid grid-cols-3 fixed bottom-0 left-0 w-full h-[72px] pt-1" style={{ borderRadius: '24px 24px 0px 0px' }}>
+        <Card className="grid grid-cols-3 fixed bottom-0 left-0 w-full h-[72px] pt-1" style={{ borderRadius: '24px 24px 0px 0px', zIndex: 50 }}>
             <BottomNavItem itemKey="people" selected={selectedState.get({ noproxy: true })} title="People" icon="gallery" onClick={onItemClick('people')} />
             <BottomNavItem itemKey="city" selected={selectedState.get({ noproxy: true })} title="City" icon="music" onClick={onItemClick('city')} />
             <BottomNavItem itemKey="settings" selected={selectedState.get({ noproxy: true })} title="Settings" icon="video" onClick={onItemClick('settings')} />
